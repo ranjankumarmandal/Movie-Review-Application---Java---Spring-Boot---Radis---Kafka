ed94f03c24be7b0e3a8640581574ec2d88c03c45
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2023-07-30 09:58:29 +0530

    updated service MovieResponse file

--- src/main/java/com/example/moviereview/service/response/MovieResponse.java
+++ src/main/java/com/example/moviereview/service/response/MovieResponse.java
@@ -1,4 +1,21 @@
 package com.example.moviereview.service.response;
 
+import com.example.moviereview.domain.Genre;
+import lombok.*;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class MovieResponse {
+
+    private String title;
+    private Genre genre;
+    private Double rating;
+    private List<ReviewResponse> reviews;
+
+
 }
