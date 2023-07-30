37710f878fad18f0b75c646847f7d43abf5f1833
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2023-07-30 11:09:43 +0530

    completed component ReviewController

---
+++ src/main/java/com/example/moviereview/contoller/ReviewController.java
@@ -0,0 +1,22 @@
+package com.example.moviereview.contoller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/review")
+public class ReviewController {
+
+    @Autowired
+    ReviewService reviewService;
+
+    @PostMapping("/add")
+    public void addReview(@RequestBody ReviewRequest reviewRequest){
+        reviewService.addReview(reviewRequest.toReview());
+    }
+
+    @GetMapping("/find")
+    public ReviewResponse getReview(@RequestParam Long reviewId){
+        return reviewService.getReviewById(reviewId);
+    }
+}
