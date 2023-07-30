52e8caa4f09cd02acf28a747647dcc58c7e30a12
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2023-07-30 11:11:39 +0530

    completed service layer implementation for ReviewService

---
+++ src/main/java/com/example/moviereview/service/utils/ReviewService.java
@@ -0,0 +1,39 @@
+package com.example.moviereview.service.utils;
+
+import com.example.moviereview.domain.Movie;
+import com.example.moviereview.domain.Review;
+import com.example.moviereview.repository.MovieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class ReviewService {
+
+    @Autowired
+    private ReviewRepository reviewRepository;
+
+    @Autowired
+    private MovieRepository movieRepository;
+
+    public void addReview(Review review) {
+        Movie movie=movieRepository.findById(review.getMovie().getId()).orElse(null);
+        reviewRepository.save(review);
+        //need to optimized
+        //exception handling.
+        if(movie!=null) {
+            Double average = reviewRepository.getReviewAverage(movie.getId());
+            movie.setRating(average);
+            movieRepository.save(movie);
+        }
+
+    }
+
+    public ReviewResponse getReviewById(Long reviewId) {
+
+        Optional<Review> review= reviewRepository.findById(reviewId);
+        return review.map(Review::toReviewResponse).orElse(null);
+
+    }
+}
