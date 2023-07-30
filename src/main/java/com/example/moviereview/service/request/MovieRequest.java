23138ac71bf9dfe88c7b54c40346a645aeb94bfa
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2023-07-30 11:25:48 +0530

    created a utility MovieRequest for AdminController implementaion

---
+++ src/main/java/com/example/moviereview/service/request/MovieRequest.java
@@ -0,0 +1,25 @@
+package com.example.moviereview.service.request;
+
+import com.example.moviereview.domain.Genre;
+import com.example.moviereview.domain.Movie;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class MovieRequest {
+
+    private String title;
+    private Genre genre;
+
+    public Movie toMovie(){
+        return Movie.builder().title(title).genre(genre).rating(0.0).build();
+    }
+
+
+}
